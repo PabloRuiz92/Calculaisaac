24d86d0fd8fb73df89f8cc77fe0f758ed00ba661
PabloRuiz <pabloruiz1992@outlook.com.ar>
2022-10-25 15:05:13 -0300

    implementados botones

--- CalculaIsaac2.py
+++ CalculaIsaac2.py
@@ -20,13 +20,13 @@ class Application(Frame):
         self.cuadro_nombre.grid(row=1, column=1)
 
         #Label y Entry stats-------------------
-        self.vida=IntVar()
-        self.damage=IntVar()
-        self.tears=IntVar()
-        self.shotSpd=IntVar()
-        self.rango=IntVar()
-        self.speed=IntVar()
-        self.luck=IntVar()
+        self.vida=IntVar(value=0)
+        self.damage=DoubleVar(value=0)
+        self.tears=DoubleVar(value=0)
+        self.shotSpd=IntVar(value=0)
+        self.rango=DoubleVar(value=0)
+        self.speed=DoubleVar(value=0)
+        self.luck=DoubleVar(value=0)
         self.variables_stat = [self.vida,self.damage,self.tears,self.shotSpd,self.rango,self.speed,self.luck]
         self.status_lista =["vida","damage","tears","shotSpd","rango","speed","luck"]
         self.label_stat = []
@@ -48,7 +48,7 @@ class Application(Frame):
         self.frame_radiales_personaje.grid()
 
         self.personaje=IntVar(value=0)
-        self.personajes_lista =[Isaac,Madgalene,Cain]
+        self.personajes_lista =[Isaac,Madgalene,Cain,Judas,DarkJudas,BlueBaby,Eve,Samson]
         self.radio_personaje=[]
         for caracter in range(len(self.personajes_lista)):
             self.radio_personaje.append(Radiobutton(
@@ -61,7 +61,7 @@ class Application(Frame):
 
         #Botones de Item--------------
         self.frame_botones_item=Frame(root)
-        self.frame_botones_item.grid( column=0, row=7, columnspan=2)
+        self.frame_botones_item.grid(column=0, row=7, columnspan=2)
 
         self.row_contador = 0
         column_contador = 5
@@ -69,8 +69,7 @@ class Application(Frame):
         self.img_boton=[]
         for item in items:
             self.img_boton.append(PhotoImage(file=(items[item]['ruta'])))
-            self.botones_item.append(Button(self.frame_botones_item, 
-                                            textvariable=items[item]["nombre"],
+            self.botones_item.append(Button(self.frame_botones_item,
                                             image=self.img_boton[item], 
                                             command=lambda item = item: self.stat_up(item)))
             self.botones_item[item].grid(row=0, column=column_contador)
@@ -88,29 +87,18 @@ class Application(Frame):
         self.label_image.image = imagen_personaje
 
     def stat_up(self, item):
-        print(item)
-        if int(self.vida.get()) < 12:
-            self.vida.set(int(self.vida.get()) + 1)
-
-
-"""
-def statUp():
-    #item = dinnerButton.cget("variable")
-    #chara = "chara" + (cuadroNombre.get())
-    if int(vidaEntry.get()) < 12:
-        vidaEntry.set(item["vida"])
-
-
-def healtup(item):
-    #item = dinnerButton.cget("variable")
-    
-    for valor in item:
-        if valor == "vida":
-            vidaEntry.set(int(vidaEntry.get()) + int(item[valor]))
-    
-    if int(vidaEntry.get()) < 12:
-        vidaEntry.set(int(vidaEntry.get()) + 1)
-"""
+        for valor in items[item]:
+            if valor in self.status_lista:
+                index = self.status_lista.index(valor)
+                self.valor = self.variables_stat[index]
+                cambio=(self.valor.get()+items[item][valor])
+                self.valor.set(cambio)
+                if self.vida.get() > 12:
+                    self.vida.set(12)
+                if self.tears.get() > 120:
+                    self.tears.set(120)
+                if self.speed.get() > 2.0:
+                    self.speed.set(2.0)   
 
 #Main-------------------
 root=Tk()
--- Items.py
+++ Items.py
@@ -1,12 +1,15 @@
 items={
     0 : {
-        "nombre": "dinner",
-        "vida": -1,
+        "nombre": "Dinner",
+        "vida": 1,
+        "damage": 5.2,
+        "speed": 0.2,
         "ruta": "Items\Collectible_Dinner_icon.png"
     },
     1 : {
         "nombre": "Aries",
         "vida": 3,
+        "damage": 5,
         "ruta": "Items\Collectible_Aries_icon.png"
     }
 }
